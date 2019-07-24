5303aeb548ef0b999d6a78736d80a752ff53557b
Thivanka <thivankanimesh@hotmail.com>
2019-07-24 19:49:34 +0530

    Did admin table initial migration

---
+++ database/migrations/2019_07_24_141126_create_admin_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->string('email',100)->unique();
+            $table->string('password',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
